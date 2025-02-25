759c01a74e42629c5837b32be1c04b73d46804f6
Aleksandr Chernov <chernovaleksander81@yandex.ru>
2025-02-25 13:21:07 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Статус сборки](https://ci.appveyor.com/api/projects/status/ivcbxe73hxa59ydf?svg=true)](https://ci.appveyor.com/project/AleksandrChernov81/selenide)
---
+++ src/test/java/ru/netology/RegistrationTest.java
@@ -0,0 +1,272 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class RegistrationTest {
+    public static String getLocalDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy", new Locale("ru")));
+    }
+
+    @BeforeAll
+    public static void openRegistrationPage() {
+        Configuration.headless = true;
+        open("http://localhost:9999/");
+    }
+
+
+    @Test
+    void shouldTestNotValidCity() {
+        String planningDate = getLocalDate(5);
+        $x("//input[@placeholder=\"Город\"]").val("Минск");
+        $x("//input[@placeholder=\"Дата встречи\"]").doubleClick().sendKeys(planningDate);
+        $x("//input[@name=\"name\"]").val("Иванов Иван");
+        $x("//*[@data-test-id=\"phone\"]/span/span/input").val("+79998887766");
+        $x("//*[@class=\"checkbox__text\"]").click();
+        $x("//*[@class=\"button__text\"]").click();
+        $(withText("Доставка в выбранный город недоступна")).should(visible, Duration.ofSeconds(5));
+        $x("//*[@data-test-id=\"notification\"]").shouldNot(visible, Duration.ofSeconds(15));
+    }
+    @Test
+    void shouldTestCityEnglish() {
+        String planningDate = getLocalDate(6);
+        $x("//input[@placeholder=\"Город\"]").val("Moscow");
+        $x("//input[@placeholder=\"Дата встречи\"]").doubleClick().sendKeys(planningDate);
+        $x("//input[@name=\"name\"]").val("Иван Иванов");
+        $x("//*[@data-test-id=\"phone\"]/span/span/input").val("+79998887766");
+        $x("//*[@class=\"checkbox__text\"]").click();
+        $x("//*[@class=\"button__text\"]").click();
+        $(withText("Доставка в выбранный город недоступна")).should(visible, Duration.ofSeconds(5));
+        $x("//*[@data-test-id=\"notification\"]").shouldNot(visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    void shouldTestCityEmpty() {
+        String planningDate = getLocalDate(6);
+        $x("//input[@placeholder=\"Город\"]").val("");
+        $x("//input[@placeholder=\"Дата встречи\"]").doubleClick().sendKeys(planningDate);
+        $x("//input[@name=\"name\"]").val("Иван Иванов");
+        $x("//*[@data-test-id=\"phone\"]/span/span/input").val("+79998887766");
+        $x("//*[@class=\"checkbox__text\"]").click();
+        $x("//*[@class=\"button__text\"]").click();
+        $(withText("Поле обязательно")).should(visible, Duration.ofSeconds(5));
+        $x("//*[@data-test-id=\"notification\"]").shouldNot(visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    void shouldTestCityWithSpecSymbols() {
+        String planningDate = getLocalDate(6);
+        $x("//input[@placeholder=\"Город\"]").val("Санкт_Петербург");
+        $x("//input[@placeholder=\"Дата встречи\"]").doubleClick().sendKeys(planningDate);
+        $x("//input[@name=\"name\"]").val("Иван Иванов");
+        $x("//*[@data-test-id=\"phone\"]/span/span/input").val("79998887766");
+        $x("//*[@class=\"checkbox__text\"]").click();
+        $x("//*[@class=\"button__text\"]").click();
+        $(withText("недоступна")).should(visible, Duration.ofSeconds(5));
+        $x("//*[@data-test-id=\"notification\"]").shouldNot(visible, Duration.ofSeconds(15));
+    }
+
+
+    @Test
+    void shouldTestDoubleFirstName() {
+        String planningDate = getLocalDate(6);
+        $x("//input[@placeholder=\"Город\"]").val("Санкт-Петербург");
+        $x("//input[@placeholder=\"Дата встречи\"]").doubleClick().sendKeys(planningDate);
+        $x("//input[@name=\"name\"]").val("Иван Иванов-Петров");
+        $x("//*[@data-test-id=\"phone\"]/span/span/input").val("+79998887766");
+        $x("//*[@class=\"checkbox__text\"]").click();
+        $x("//*[@class=\"button__text\"]").click();
+        $x("//*[@data-test-id=\"notification\"]").should(visible, Duration.ofSeconds(15));
+        $x("//*[@class='notification__content']").
+                shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15));
+    }
+
+    @Test
+    void shouldTestNameWithNum() {
+        String planningDate = getLocalDate(6);
+        $x("//input[@placeholder=\"Город\"]").val("Санкт-Петербург");
+        $x("//input[@placeholder=\"Дата встречи\"]").doubleClick().sendKeys(planningDate);
+        $x("//input[@name=\"name\"]").val("14567 1564");
+        $x("//*[@data-test-id=\"phone\"]/span/span/input").val("+79998887766");
+        $x("//*[@class=\"checkbox__text\"]").click();
+        $x("//*[@class=\"button__text\"]").click();
+        $(withText("только русские буквы, пробелы и дефисы")).should(visible);
+        $x("//*[@data-test-id=\"notification\"]").shouldNot(visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    void shouldTestNameEnglish() {
+        String planningDate = getLocalDate(6);
+        $x("//input[@placeholder=\"Город\"]").val("Санкт-Петербург");
+        $x("//input[@placeholder=\"Дата встречи\"]").doubleClick().sendKeys(planningDate);
+        $x("//input[@name=\"name\"]").val("Ivan Ivanov");
+        $x("//*[@data-test-id=\"phone\"]/span/span/input").val("+79998887766");
+        $x("//*[@class=\"checkbox__text\"]").click();
+        $x("//*[@class=\"button__text\"]").click();
+        $(withText("только русские буквы, пробелы и дефисы")).should(visible);
+        $x("//*[@data-test-id=\"notification\"]").shouldNot(visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    void shouldTestNameWithSpecSymbols() {
+        String planningDate = getLocalDate(6);
+        $x("//input[@placeholder=\"Город\"]").val("Санкт-Петербург");
+        $x("//input[@placeholder=\"Дата встречи\"]").doubleClick().sendKeys(planningDate);
+        $x("//input[@name=\"name\"]").val("Иван Иванов!");
+        $x("//*[@data-test-id=\"phone\"]/span/span/input").val("+79998887766");
+        $x("//*[@class=\"checkbox__text\"]").click();
+        $x("//*[@class=\"button__text\"]").click();
+        $(withText("только русские буквы, пробелы и дефисы")).should(visible);
+        $x("//*[@data-test-id=\"notification\"]").shouldNot(visible, Duration.ofSeconds(15));
+    }
+    @Test
+
+    void shouldTestPhoneWithoutPlus() {
+        String planningDate = getLocalDate(90);
+        $x("//input[@placeholder=\"Город\"]").val("Санкт-Петербург");
+        $x("//input[@placeholder=\"Дата встречи\"]").doubleClick().sendKeys(planningDate);
+        $x("//input[@name=\"name\"]").val("Иван Иванов");
+        $x("//*[@data-test-id=\"phone\"]/span/span/input").val("89998887766");
+        $x("//*[@class=\"checkbox__text\"]").click();
+        $x("//*[@class=\"button__text\"]").click();
+        $(withText("11 цифр")).should(visible, Duration.ofSeconds(5));
+        $x("//*[@data-test-id=\"notification\"]").shouldNot(visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    void shouldTestPhoneWithOneNumber() {
+        String planningDate = getLocalDate(90);
+        $x("//input[@placeholder=\"Город\"]").val("Санкт-Петербург");
+        $x("//input[@placeholder=\"Дата встречи\"]").doubleClick().sendKeys(planningDate);
+        $x("//input[@name=\"name\"]").val("Иван Иванов");
+        $x("//*[@data-test-id=\"phone\"]/span/span/input").val("+7999888776");
+        $x("//*[@class=\"checkbox__text\"]").click();
+        $x("//*[@class=\"button__text\"]").click();
+        $(withText("11 цифр")).should(visible, Duration.ofSeconds(5));
+        $x("//*[@data-test-id=\"notification\"]").shouldNot(visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    void shouldTestPhoneWithSpecSymbols() {
+        String planningDate = getLocalDate(90);
+        $x("//input[@placeholder=\"Город\"]").val("Санкт-Петербург");
+        $x("//input[@placeholder=\"Дата встречи\"]").doubleClick().sendKeys(planningDate);
+        $x("//input[@name=\"name\"]").val("Иван Иванов");
+        $x("//*[@data-test-id=\"phone\"]/span/span/input").val("+7(999)-888-77 66");
+        $x("//*[@class=\"checkbox__text\"]").click();
+        $x("//*[@class=\"button__text\"]").click();
+        $(withText("11 цифр")).should(visible, Duration.ofSeconds(5));
+        $x("//*[@data-test-id=\"notification\"]").shouldNot(visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    void shouldTestPhoneEmpty() {
+        String planningDate = getLocalDate(90);
+        $x("//input[@placeholder=\"Город\"]").val("Санкт-Петербург");
+        $x("//input[@placeholder=\"Дата встречи\"]").doubleClick().sendKeys(planningDate);
+        $x("//input[@name=\"name\"]").val("Иван Иванов");
+        $x("//*[@data-test-id=\"phone\"]/span/span/input").val("");
+        $x("//*[@class=\"checkbox__text\"]").click();
+        $x("//*[@class=\"button__text\"]").click();
+        $(withText("обязательно для заполнения")).should(visible, Duration.ofSeconds(5));
+        $x("//*[@data-test-id=\"notification\"]").shouldNot(visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    void shouldTestNextDayMeeting() {
+        String planningDate = getLocalDate(1);
+        $x("//input[@placeholder=\"Город\"]").val("Санкт-Петербург");
+        $x("//input[@placeholder=\"Дата встречи\"]").doubleClick().sendKeys(planningDate);
+        $x("//input[@name=\"name\"]").val("Иван Иванов");
+        $x("//*[@data-test-id=\"phone\"]/span/span/input").val("+79998887766");
+        $x("//*[@class=\"checkbox__text\"]").click();
+        $x("//*[@class=\"button__text\"]").click();
+        $(withText("дату невозможен")).should(visible, Duration.ofSeconds(5));
+        $x("//*[@data-test-id=\"notification\"]").shouldNot(visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    void shouldTestPlus0days() {
+        String planningDate = getLocalDate(0);
+        $x("//input[@placeholder=\"Город\"]").val("Санкт-Петербург");
+        $x("//input[@placeholder=\"Дата встречи\"]").doubleClick().sendKeys(planningDate);
+        $x("//input[@name=\"name\"]").val("Иван Иванов");
+        $x("//*[@data-test-id=\"phone\"]/span/span/input").val("+79998887766");
+        $x("//*[@class=\"checkbox__text\"]").click();
+        $x("//*[@class=\"button__text\"]").click();
+        $(withText("дату невозможен")).should(visible, Duration.ofSeconds(5));
+        $x("//*[@data-test-id=\"notification\"]").shouldNot(visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    void shouldTestMinus5Days() {
+        String planningDate = getLocalDate(-5);
+        $x("//input[@placeholder=\"Город\"]").val("Санкт-Петербург");
+        $x("//input[@placeholder=\"Дата встречи\"]").doubleClick().sendKeys(planningDate);
+        $x("//input[@name=\"name\"]").val("Иван Иванов");
+        $x("//*[@data-test-id=\"phone\"]/span/span/input").val("+79998887766");
+        $x("//*[@class=\"checkbox__text\"]").click();
+        $x("//*[@class=\"button__text\"]").click();
+        $(withText("дату невозможен")).should(visible, Duration.ofSeconds(5));
+        $x("//*[@data-test-id=\"notification\"]").shouldNot(visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    void shouldTestFebruaryDays() {
+        $x("//input[@placeholder=\"Город\"]").val("Санкт-Петербург");
+        $x("//input[@placeholder=\"Дата встречи\"]").doubleClick().sendKeys("30.02.2023");
+        $x("//input[@name=\"name\"]").val("Иван Иванов");
+        $x("//*[@data-test-id=\"phone\"]/span/span/input").val("+79998887766");
+        $x("//*[@class=\"checkbox__text\"]").click();
+        $x("//*[@class=\"button__text\"]").click();
+        $(withText("Неверно введена дата")).should(visible, Duration.ofSeconds(5));
+        $x("//*[@data-test-id=\"notification\"]").shouldNot(visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    void shouldTestUncheckedBox() {
+        String planningDate = getLocalDate(4);
+        $x("//input[@placeholder=\"Город\"]").val("Санкт-Петербург");
+        $x("//input[@placeholder=\"Дата встречи\"]").doubleClick().sendKeys(planningDate);
+        $x("//input[@name=\"name\"]").val("Иван Иванов");
+        $x("//*[@data-test-id=\"phone\"]/span/span/input").val("+79998887766");
+        $x("//*[@class=\"checkbox__text\"]").doubleClick();
+        $x("//*[@class=\"button__text\"]").click();
+        $(".input_invalid[data-test-id=\"agreement\"]").should(exist);
+        $x("//*[@data-test-id=\"notification\"]").shouldNot(visible, Duration.ofSeconds(15));
+
+    }
+
+    @Test
+    void shouldTestCheckedCheckedBox() {
+        String planningDate = getLocalDate(4);
+        $x("//input[@placeholder=\"Город\"]").val("Санкт-Петербург");
+        $x("//input[@placeholder=\"Дата встречи\"]").doubleClick().sendKeys(planningDate);
+        $x("//input[@name=\"name\"]").val("Иван Иванов");
+        $x("//*[@data-test-id=\"phone\"]/span/span/input").val("+79998887766");
+        $x("//*[@class=\"checkbox__text\"]").click();
+        $x("//*[@class=\"button__text\"]").click();
+        $("[data-test-id=agreement].checkbox_checked").should(exist);
+        $x("//*[@data-test-id=\"notification\"]").should(visible, Duration.ofSeconds(15));
+        $x("//*[@class='notification__content']").
+                shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15));
+    }
+
+}
+
+
+
+
+
