2e94f4a33b7ce0e669837a627bcb596b29bc28e5
Aleksandr Chernov <chernovaleksander81@yandex.ru>
2025-03-04 14:26:44 +0300

    change to build.gradle

--- src/test/java/ru/netology/RegistrationTest.java
+++ src/test/java/ru/netology/RegistrationTest.java
@@ -1,6 +1,10 @@
 package ru.netology;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -21,8 +25,9 @@ public class RegistrationTest {
 
     public static String getLocalDate(int days) {
         return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy", new Locale("ru")));
-    }
 
+
+    }
     @BeforeEach
     void openRegistrationPage() {
         open("http://localhost:9999/");
